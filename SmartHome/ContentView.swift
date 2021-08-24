861bdf46fb07984e700926c1f2d378352bbbecb1
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-08-24 18:47:46 +0300

    Add tabbar and screen placeholders.

--- SmartHome/ContentView.swift
+++ SmartHome/ContentView.swift
@@ -8,10 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	enum RootPage {
+	case home
+	case rooms
+	case devices
+	case settings
+	}
+	
+	@State private var currentTab: RootPage = .home
+	
     var body: some View {
     	NavigationView {
-    		HomeView()
+    		TabView(selection: $currentTab) {
+    			HomeView()
+    				.tag(RootPage.home)
+    				.tabItem {
+    					VStack {
+    						Image(systemName: "house.fill")
+    							.font(.system(size: 25))
+    						Text("Home")
+    							.font(.system(size:12, weight: .bold))
+						}
+					}
+				
+				Text("Rooms List Under Construction")
+    				.tag(RootPage.rooms)
+    				.tabItem {
+    					Image(systemName: "square.grid.2x2.fill")
+    							.font(.system(size: 25))
+						Text("Rooms")
+							.font(.system(size:12, weight: .bold))
+					}
+					
+				Text("Device List Under Construction")
+    				.tag(RootPage.devices)
+    				.tabItem {
+    					Image(systemName: "hifispeaker.fill")
+    							.font(.system(size: 25))
+						Text("Devices")
+							.font(.system(size:12, weight: .bold))
+					}
+				
+				Text("Settings Under Construction")
+    				.tag(RootPage.settings)
+    				.tabItem {
+    					Image(systemName: "gearshape.fill")
+    							.font(.system(size: 25))
+						Text("Settings")
+							.font(.system(size:12, weight: .bold))
+					}
+				
+    		}
     	}
+    	.navigationViewStyle(StackNavigationViewStyle())
 	}
 }
 
