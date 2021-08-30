1f0deae29908f62e20bb2efd286cb554cc44d285
dmitriy-shmilo <dmitriy.shmilo@gmail.com>
2021-08-31 00:04:33 +0300

    Sticky top navigation on details screen.

--- SmartHome/RoomDetails/RoomDetailsView.swift
+++ SmartHome/RoomDetails/RoomDetailsView.swift
@@ -16,91 +16,99 @@ struct RoomDetailsView: View {
 	
 	var body: some View {
 		GeometryReader { proxy in
-			ScrollView {
-				VStack {
-					HStack {
-						Button(action: {
-							presentation.wrappedValue.dismiss()
-						}) {
-							Image(systemName: "chevron.left")
-								.foregroundColor(.font)
-								.font(.system(size: 16))
-						}
-						
-						Spacer()
-						Text(room.name)
-							.foregroundColor(.font)
-							.font(.system(size: 18, weight: .semibold))
-						Spacer()
-					}
-					.padding()
-					.padding(.top, proxy.safeAreaInsets.top)
-					
-					ScrollView(.horizontal, showsIndicators: false) {
-						HStack {
-							ForEach(room.sensors.indices) { index in
-								let sensor = room.sensors[index]
-								SensorDataCardView(
-									data: "\(floor(sensor.data * counterProgress)) \(sensor.type.unitLabel)",
-									subtitle: sensor.type.name,
-									background: index.isMultiple(of: 2)
-										? Color.dataBlue
-										: Color.dataOrange
-								)
+			ZStack (alignment: .top) {
+				ScrollView {
+					VStack {
+						ScrollView(.horizontal, showsIndicators: false) {
+							HStack {
+								ForEach(room.sensors.indices) { index in
+									let sensor = room.sensors[index]
+									SensorDataCardView(
+										data: "\(floor(sensor.data * counterProgress)) \(sensor.type.unitLabel)",
+										subtitle: sensor.type.name,
+										background: index.isMultiple(of: 2)
+											? Color.dataBlue
+											: Color.dataOrange
+									)
+								}
 							}
-						}
-						.padding(.leading, 32 + proxy.safeAreaInsets.leading)
-						.padding(.trailing, 32 + proxy.safeAreaInsets.trailing)
-						.onAppear {
-							Timer.scheduledTimer(
-								withTimeInterval: 0.75 / counterSteps, repeats: true
-							) { timer in
-								counterProgress += 1.0 / counterSteps
-								if counterProgress >= 1.0 {
-									timer.invalidate()
+							.padding(.leading, 32 + proxy.safeAreaInsets.leading)
+							.padding(.trailing, 32 + proxy.safeAreaInsets.trailing)
+							.onAppear {
+								Timer.scheduledTimer(
+									withTimeInterval: 0.75 / counterSteps, repeats: true
+								) { timer in
+									counterProgress += 1.0 / counterSteps
+									if counterProgress >= 1.0 {
+										timer.invalidate()
+									}
 								}
 							}
 						}
-					}
-					.padding(.leading, -proxy.safeAreaInsets.leading)
-					.padding(.trailing, -proxy.safeAreaInsets.trailing)
-					.ignoresSafeArea()
-					
-					Text("Power consumption")
-						.foregroundColor(.font)
-						.font(.system(size:18, weight: .bold))
-						.frame(maxWidth: .infinity, alignment: .leading)
-						.padding(.top)
-						.padding(.leading, 32 + proxy.safeAreaInsets.leading)
-						.padding(.trailing, 32 + proxy.safeAreaInsets.trailing)
-					
-					PowerChartView(
-						lastWeekData: room.consumption[7..<14].map { CGFloat($0) },
-						currentWeekData: room.consumption[0..<7].map { CGFloat($0) }
-					)
-					.padding(.leading, 32 + proxy.safeAreaInsets.leading)
-					.padding(.trailing, 32 + proxy.safeAreaInsets.trailing)
-					
-					Text("Devices")
-						.foregroundColor(.font)
-						.font(.system(size:18, weight: .bold))
-						.frame(maxWidth: .infinity, alignment: .leading)
-						.padding(.top)
-						.padding(.leading, 32 + proxy.safeAreaInsets.leading)
-						.padding(.trailing, 32 + proxy.safeAreaInsets.trailing)
-					
-					ForEach(room.devices) { device in
-						DeviceCardView(device: device, room: room)
+						.padding(.leading, -proxy.safeAreaInsets.leading)
+						.padding(.trailing, -proxy.safeAreaInsets.trailing)
+						.ignoresSafeArea()
+						
+						Text("Power consumption")
+							.foregroundColor(.font)
+							.font(.system(size:18, weight: .bold))
+							.frame(maxWidth: .infinity, alignment: .leading)
+							.padding(.top)
 							.padding(.leading, 32 + proxy.safeAreaInsets.leading)
 							.padding(.trailing, 32 + proxy.safeAreaInsets.trailing)
+						
+						PowerChartView(
+							lastWeekData: room.consumption[7..<14].map { CGFloat($0) },
+							currentWeekData: room.consumption[0..<7].map { CGFloat($0) }
+						)
+						.padding(.leading, 32 + proxy.safeAreaInsets.leading)
+						.padding(.trailing, 32 + proxy.safeAreaInsets.trailing)
+						
+						Text("Devices")
+							.foregroundColor(.font)
+							.font(.system(size:18, weight: .bold))
+							.frame(maxWidth: .infinity, alignment: .leading)
+							.padding(.top)
+							.padding(.leading, 32 + proxy.safeAreaInsets.leading)
+							.padding(.trailing, 32 + proxy.safeAreaInsets.trailing)
+						
+						ForEach(room.devices) { device in
+							DeviceCardView(device: device, room: room)
+								.padding(.leading, 32 + proxy.safeAreaInsets.leading)
+								.padding(.trailing, 32 + proxy.safeAreaInsets.trailing)
+						}
 					}
+					.padding(.leading, proxy.safeAreaInsets.leading)
+					.padding(.trailing, proxy.safeAreaInsets.trailing)
+					.padding(.top, proxy.safeAreaInsets.top + 64)
 				}
-				.padding(.leading, proxy.safeAreaInsets.leading)
-				.padding(.trailing, proxy.safeAreaInsets.trailing)
+				.background(Color.background.ignoresSafeArea())
+				.navigationBarHidden(true)
+				
+				HStack {
+					Button(action: {
+						presentation.wrappedValue.dismiss()
+					}) {
+						Image(systemName: "chevron.left")
+							.foregroundColor(.font)
+							.font(.system(size: 16))
+					}
+					.padding(.top, proxy.safeAreaInsets.top + 16)
+					.padding(.leading, proxy.safeAreaInsets.leading + 16)
+					.padding(.bottom, 16)
+					
+					Spacer()
+					Text(room.name)
+						.foregroundColor(.font)
+						.font(.system(size: 18, weight: .semibold))
+						.padding(.top, proxy.safeAreaInsets.top + 16)
+						.padding(.trailing, proxy.safeAreaInsets.trailing + 16)
+						.padding(.bottom, 16)
+					Spacer()
+				}
+				.background(Color.background.opacity(0.5).ignoresSafeArea())
 			}
 			.ignoresSafeArea()
-			.background(Color.background.ignoresSafeArea())
-			.navigationBarHidden(true)
 		}
 	}
 }
